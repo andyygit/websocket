83524c504a148785b5afcfd5139238594b58491a
andyygit <android.bartos@gmail.com>
2024-07-17 15:52:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (data) => {
+    console.log('received %s', data);
+    ws.send(`${data}`);
+  });
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
